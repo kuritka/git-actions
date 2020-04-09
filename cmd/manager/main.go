ae8825858a68ed51d336bb315b00ee9cdc4bc2c0
kuritka <kuritka@gmail.com>
2020-04-09 14:36:07 +0200

    first commit

---
+++ cmd/manager/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "github.com/kuritka/gext/log"
+
+var logger = log.Log
+
+func main(){
+	logger.Msg("panic!")
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/git-actions
+
+go 1.14
+
+require github.com/kuritka/gext v0.0.2
